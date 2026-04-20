4cd46aa7c22832ef5ee6d7200bf7f8f603c010d2
Lachie King <kingy00112233@gmail.com>
2026-04-20 11:20:37 +1000

    update task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,6 +8,9 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple containing a start and end index for pagination.
     """
+    if page <= 0 or page_size <= 0:
+        return (0, 0)
+
     start_index = (page - 1) * page_size
     end_index = page * page_size
 
