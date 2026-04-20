a89ab045406b0cf84225da9c662a45dcaf9a16ee
Lachie King <kingy00112233@gmail.com>
2026-04-20 11:06:17 +1000

    update task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,8 +2,9 @@
 """
 Main module for pagination helper function.
 """
+from typing import Tuple
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple containing a start and end index for pagination.
     """
