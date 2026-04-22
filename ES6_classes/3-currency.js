6f3f91a5fc3722ea80b65467c39c6bec37b6fbb5
Lachie King <kingy00112233@gmail.com>
2026-04-22 15:00:27 +1000

    add task 4

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+
+get code() {
+    return this._code;
+}
+
+get name() {
+    return this._name;
+}
+
+set code(value) {
+    if (typeof value !== 'string') {
+        throw new TypeError('Code must be a string')
+    } else {
+        this._code = value;
+    }
+}
+
+set name(value) {
+    if (typeof value !== 'string') {
+        throw new TypeError('Name must be a string')
+    } else {
+        this._code = value;
+    }
+}
+}
