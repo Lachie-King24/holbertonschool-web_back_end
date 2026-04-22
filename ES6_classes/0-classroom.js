bf71f03c7aab032c7c434c998bb479b56ee0a25f
Lachie King <kingy00112233@gmail.com>
2026-04-22 14:44:38 +1000

    update 0-classroom.js and add 0-main to test

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,4 +1,9 @@
 class ClassRoom {
     constructor(maxStudentsSize) {
         this._maxStudentsSize = maxStudentsSize;
-    }}
+    }
+
+    get maxStudentsSize() {
+        return this._maxStudentsSize;
+    }
+}
