812517ad571a6fd4c6fb271e9d9e54c8a46f817a
Lachie King <kingy00112233@gmail.com>
2026-04-22 14:54:01 +1000

    add task 3

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    // Getters
+    get name() {
+        return this._name;
+    }
+    get length() {
+        return this._length;
+    }
+    get students() {
+        return this._students;
+    }
+    // Setters
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+        } else {
+            this._name = value;
+        }
+    }
+    set length(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Length must be a number');
+        } else {
+            this._length = value;
+        }
+    } 
+    set students(value) {
+        if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        } else {
+            this._students = value;
+        }
+    }
+}
