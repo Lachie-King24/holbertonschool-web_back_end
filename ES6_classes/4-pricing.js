69881c82b25fa80a82bcce37c9516d7d7ca3c33f
Lachie King <kingy00112233@gmail.com>
2026-04-22 15:07:12 +1000

    add task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    // Getters
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    // Setters
+    set amount(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Amount must be a number');
+        } else {
+            this._amount = value;
+        }
+    }
+
+    set currency(value) {
+        if (!(value instanceof Currency)) {
+            throw new TypeError('Currency must be an instance of the Currency class');
+        } else {
+            this._currency = value;
+        }
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
+
+    convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
