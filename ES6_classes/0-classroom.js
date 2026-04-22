6f3fc0498ed292c0566ec225dee742edce751d83
Lachie King <kingy00112233@gmail.com>
2026-04-22 14:48:46 +1000

    add export default

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,9 +1,5 @@
-class ClassRoom {
+export default class ClassRoom {
     constructor(maxStudentsSize) {
         this._maxStudentsSize = maxStudentsSize;
-    }
-
-    get maxStudentsSize() {
-        return this._maxStudentsSize;
     }
 }
