44861897492067bc51596adbb16359e093ec1ac9
Lachie King <kingy00112233@gmail.com>
2026-04-22 14:50:52 +1000

    add task 2

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom"
+
+export default function initializeRooms() {
+    const one = new ClassRoom(19);
+    const two = new ClassRoom(20);
+    const three = new ClassRoom(34);
+    return [one, two, three];
+}
